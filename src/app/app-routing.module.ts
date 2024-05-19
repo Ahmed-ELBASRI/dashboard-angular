30dc71b93c162d6113ff3ae00de6451a2a3459d8
Karim Mokhlis <karimmokhlis16@gmail.com>
2024-05-19 14:05:19 +0100

    paiement componenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditprofileComponent } from './vendeur/editprofile/editprofile.component';
 import { CommandsComponent } from './elements/commands/commands.component';
+import { PlansComponent } from './membership/plans/plans.component';
+import { PaiementComponent } from './membership/paiement/paiement.component';
 
 const routes: Routes = [ 
   { path: 'editprofile', component: EditprofileComponent },
-  { path: 'Commands' , component: CommandsComponent }
+  { path: 'Commands' , component: CommandsComponent },
+  { path: 'plans' , component: PlansComponent },
+  { path: 'paiement' , component: PaiementComponent  }
+
 ];
 
 @NgModule({
